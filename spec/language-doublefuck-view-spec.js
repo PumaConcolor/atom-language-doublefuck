cb23216f29ddbab2bc84cea071074973f63c3d1d
PumaConcolor <puma.9789@gmail.com>
2018-12-10 17:33:23 +0100

    asdf

---
+++ spec/language-doublefuck-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageDoublefuckView from '../lib/language-doublefuck-view';
+
+describe('LanguageDoublefuckView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
